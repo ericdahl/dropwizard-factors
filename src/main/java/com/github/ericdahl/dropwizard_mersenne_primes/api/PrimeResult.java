c0ca77a59a0ede858912cfec526a63fd4706cd6c
Eric Dahl <eric.c.dahl@gmail.com>
2014-11-16 10:18:32 -0800

    add test for PrimeResource

--- src/main/java/com/github/ericdahl/dropwizard_mersenne_primes/api/PrimeResult.java
+++ src/main/java/com/github/ericdahl/dropwizard_mersenne_primes/api/PrimeResult.java
@@ -2,13 +2,16 @@ package com.github.ericdahl.dropwizard_mersenne_primes.api;
 
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Objects;
 
 public class PrimeResult {
     private long n;
     private boolean prime;
 
     @JsonCreator
-    public PrimeResult(final long n, final boolean prime) {
+    public PrimeResult(@JsonProperty("n") final long n, @JsonProperty("prime") final boolean prime) {
         this.n = n;
         this.prime = prime;
     }
@@ -19,5 +22,28 @@ public class PrimeResult {
 
     public long getN() {
         return n;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PrimeResult that = (PrimeResult) o;
+
+        return Objects.equals(n, that.n) && Objects.equals(prime, that.prime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(n, prime);
+    }
+
+    @Override
+    public String toString() {
+        return "PrimeResult{" +
+                "prime=" + prime +
+                ", n=" + n +
+                '}';
     }
 }
